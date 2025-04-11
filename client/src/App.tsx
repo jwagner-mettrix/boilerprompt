174a4f064f65c927d91a3a1ce2229bd6ea6ee013
jwagner-mettrix <jwagner@mettrix.com>
2025-04-11 09:38:27 -0400

    working dark mode toggle, system event listener

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Button } from '@/components/ui/button'
-
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/mode-toggle'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,21 +19,13 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <ThemeProvider>
       <div className="card">
+        <ModeToggle />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <Button onClick={fetchRandomNumber} className="ml-4">
+        <Button onClick={fetchRandomNumber} variant="outline" className="ml-4w">
           Get Random Number
         </Button>
         <p>
@@ -45,7 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   )
 }
 
